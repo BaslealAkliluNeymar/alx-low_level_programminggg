289bc72a38883d99cfd170d169f67c4089b23ab3
BaslealAkliluNeymar <baslealaklilu@gmail.com>
2022-04-21 03:07:20 -0700

    dfdf

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* string_toupper -  changes the string to upper case
+* @c: the variable to be chnged
+* Return: the vhanges cariale is returned
+*/
+
+char *string_toupper(char *c)
+{
+int i;
+for (i = 0; c[i] != '\0'; i++)
+{
+if (c[i] > 'a' && c[i] < 'z')
+{
+c[i] = c[i] - 32;
+}
+return (c);
+}
