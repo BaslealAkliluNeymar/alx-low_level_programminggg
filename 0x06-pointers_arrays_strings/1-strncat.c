09fd628f790163472cc5f6c88abfbe3abcce7261
BaslealAkliluNeymar <baslealaklilu@gmail.com>
2022-04-21 01:08:48 -0700

    ldfkjs

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* _strncat - concatenenates towo strings
+* @dest: destination string
+* @src: Source string
+* @n: upto this number
+* Return: returns the concatenated string
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+int i,j = 0;
+while (dest[i] != '\0')
+{
+i++;
+}
+while (src[j] != '\0' && j < n)
+{
+dest[i] = src[j];
+i++;
+j++;
+}
+dest[i] = '\0';
+return (dest);
+}
