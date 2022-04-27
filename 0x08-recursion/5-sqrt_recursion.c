1cdbfbe0b02c17194e812cc7b1a145bf81de0cde
BaslealAkliluNeymar <baslealaklilu@gmail.com>
2022-04-27 09:48:39 -0700

    d

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -10,8 +10,9 @@ int actual_recursion(int n, int i);
  */
 int _sqrt_recursion(int n)
 {
-if (n < 0)
+if (n < 0){
 return (-1);
+}
 return (actual_recursion(n, 0));
 }
 
@@ -25,9 +26,11 @@ return (actual_recursion(n, 0));
  */
 int actual_recursion(int n, int i)
 {
-if (i * i > n)
+if (i * i > n){
 return (-1);
-if (i * i == n)
+}
+if (i * i == n){
 return (i);
+}
 return (actual_sqrt_recursion(n, i + 1));
 }
