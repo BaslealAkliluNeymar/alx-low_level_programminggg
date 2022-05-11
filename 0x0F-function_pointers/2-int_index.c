cddc88a0a6b682bc5a059280e634730284fb3d0d
BaslealAkliluNeymar <baslealaklilu@gmail.com>
2022-05-11 09:02:20 -0700

    dd

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,7 +1,9 @@
-#include <stdio.h>
 #include "function_pointers.h"
 /**
  * int_index - prints the index of the number in the array
+ * @array: array to search 
+ * @size: size of the array
+ * @cmp: pointer to the function
  * Return: Always(0) Success
  */
 
